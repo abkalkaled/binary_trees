7b1324d94b89b9712f00ac1b9607b3e799472c57
abkalkaled <aminukhaleedabiola@gmail.com>
2024-02-29 11:55:57 -0800

    uncle again

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL ||
 			node->parent->parent == NULL)
 		return (NULL);
-	binary_tree_t *grandparent = node->parent->parent;
+	*grandparent = node->parent->parent;
 
 	if (grandparent->left == node->parent)
 		return (grandparent->right);
